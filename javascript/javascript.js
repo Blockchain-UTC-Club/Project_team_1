1f39ff3a48a2bf98f17ad5642b53e04d7b44b844
ptanh05 <148223045+ptanh05@users.noreply.github.com>
2024-07-22 21:47:07 +0700

    pta sửa lại

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,13 +1,15 @@
-document.getElementById('loginForm').addEventListener('submit',function(event){
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded',function(){
+       document.getElementById('loginForm').addEventListener('submit',function(event){
+        event.preventDefault();
 
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
 
-    if(username === '' || password === ''){
-        alert('vui lòng điền cả tên đăng nhập va mật khẩu');
-    }
-    else{
-        alert('đăng nhập thành công');
-    }
+        if(username === '' || password === ''){
+            alert('vui lòng điền cả tên đăng nhập va mật khẩu');
+        }
+        else{
+            alert('đăng nhập thành công');
+        }
+    }); 
 });
