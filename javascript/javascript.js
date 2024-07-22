526c6abbe6b61209f3bb2106eafaf3d5f05b6f17
ptanh05 <148223045+ptanh05@users.noreply.github.com>
2024-07-22 21:24:11 +0700

    pta bổ sung

---
+++ javascript/javascript.js
@@ -0,0 +1,13 @@
+document.getElementById('loginForm').addEventListener('submit',function(event){
+    event.preventDefault();
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if(username === '' || password === ''){
+        alert('vui lòng điền cả tên đăng nhập va mật khẩu');
+    }
+    else{
+        alert('đăng nhập thành công');
+    }
+});
